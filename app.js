fd1086e69ac06301c3900f2f152e4f00b7b557a1
Ahmet Murat Acar <neistersenat@gmail.com>
2024-01-05 00:34:15 -0800

    Properly sets up app.js to scale.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+import bodyParser from "body-parser";
 import express from "express";
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// EJS setup
+app.set("view engine", "ejs");
+
+// Routes
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+	res.render("index");
 });
 
+// Starting server
 app.listen(port, () => {
 	console.log(`Runnig at port ${port}`);
-})
+});
